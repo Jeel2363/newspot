cf770eb0a85031963ff662ceac5220da08e884bb
Jeel Vadadoriya <91378183+Jeel2363@users.noreply.github.com>
2024-02-01 12:55:58 +0530

    Update NewsBoard.jsx

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -6,7 +6,7 @@ const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([])
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=583f7fde50794ef1ae397a6af90006f5`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=583f7fde50794ef1ae397a6af90006f5`
     fetch(url).then(response => response.json()).then(data => setArticles(data.articles))
   }, [category])
   return (
